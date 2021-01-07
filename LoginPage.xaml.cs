b6eda647ea28b4d39a6fe8da0a044193ffbb4ad1
elenapop <72506899+elenapop@users.noreply.github.com>
2021-01-07 12:22:50 +0200

    Add files via upload

---
+++ LoginPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using JOURNAL.Tables;
+using SQLite;
+using Xamarin.Forms;
+
+namespace JOURNAL.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new RegistrationPage());
+        }
+
+        async void Button_Clicked_1(System.Object sender, System.EventArgs e)
+        {
+            var dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "UserDatabase.db");
+            var db = new SQLiteConnection(dbpath);
+            var myquery = db.Table<RegisterUserTable>().Where(u => u.UserName.Equals(EntryUser.Text) && u.Password.Equals(EntryPassword.Text)).FirstOrDefault();
+
+            if (myquery !=null)
+            {
+                App.Current.MainPage = new NavigationPage(new HomePage()); // functia cu care se navigheaza de la o pagina la alta
+            }
+            else
+            {
+                {
+                    var result = await this.DisplayAlert("Error", "Try again", "Ok", "Close");
+
+                    if (result)
+                        await Navigation.PushAsync(new LoginPage());
+                };
+            }
+
+        }
+    }
+}
---
+++ RegistrationPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using JOURNAL.Tables;
+using SQLite;
+using Xamarin.Forms;
+
+namespace JOURNAL.Views
+{
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "UserDatabase.db");
+            var db = new SQLiteConnection(dbpath);
+            db.CreateTable<RegisterUserTable>();
+
+            var item = new RegisterUserTable()
+            {
+                UserName = EntryUserName.Text,
+                Password = EntryUserPassword.Text,
+                Email = EntryUserEmail.Text,
+
+            };
+
+            db.Insert(item);
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                var result = await this.DisplayAlert("Congratulations", "User Registration Succesful", "Ok", "Close");
+
+                if (result)
+                    await Navigation.PushAsync(new LoginPage());
+            });
+
+
+
+
+        }
+
+    }
+}
