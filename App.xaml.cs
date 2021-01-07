a5278639558fb99ccf2d4c66a02adffae205e0fe
elenapop <72506899+elenapop@users.noreply.github.com>
2021-01-07 12:18:31 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using JOURNAL.Tables;
+using JOURNAL.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace JOURNAL
+{
+    public partial class App : Application
+    {
+
+        static RegisterCityTable database;
+
+        public static RegisterCityTable Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new RegisterCityTable(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "people.db3"));
+                }
+                return database;
+            }
+        }
+
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new LoginPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ HomePage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using JOURNAL.Views;
+using JOURNAL.Tables;
+using JOURNAL;
+
+namespace JOURNAL
+{
+    public partial class HomePage : ContentPage
+    {
+
+        private object ListView;
+
+        
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            listView.ItemsSource = await App.Database.GetRegisterCityTableAsync();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(CityName.Text) && !string.IsNullOrWhiteSpace(CityCountry.Text))
+            {
+                await App.Database.SaveCityAsync(new City
+                {
+                    CityName = CityName.Text,
+                    CityCountry = int.Parse(CityCountry.Text)
+                });
+
+                CityName.Text = CityCountry.Text = string.Empty;
+                listView.ItemsSource = await App.Database.GetRegisterCityTableAsync();
+            }
+        }
+
+
+        public HomePage()
+        {
+            SetValue(NavigationPage.HasBackButtonProperty, false);
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new LoginPage());
+        }
+
+    }
+}
